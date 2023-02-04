02d776ef0c6910fb6322499bd92aa2ecc467cf17
timur <t.worck.m@gemail.com>
2023-02-04 18:45:38 +0100

    add simpl algoritm

--- SortingByChoice/Program.cs
+++ SortingByChoice/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void SelectionSort(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        int post = i;
+
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[post])
+            {
+                post = j;
+            }
+
+        }
+
+        int temp = array[i];
+        array[i] = array[post];
+        array[post] = temp;
+        // Console.WriteLine($"{i}. [{String.Join(", ", array)}]");
+    }
+}
+
+int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+SelectionSort(arr);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
