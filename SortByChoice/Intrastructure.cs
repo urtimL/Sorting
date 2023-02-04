08b076ad37f8ec0e077a424053d3ebe1ee57179d
timur <t.worck.m@gemail.com>
2023-02-04 19:43:33 +0100

    add final+ description

--- SortByChoice/Intrastructure.cs
+++ SortByChoice/Intrastructure.cs
@@ -1,6 +1,13 @@
 using static System.Console;
 public static class Intrastructure
 {
+    /// <summary>
+    /// Метод создания и заполнения массива
+    /// </summary>
+    /// <param name="size">Размер нового массива</param>
+    /// <param name="min">Нижняя граница заполнения</param>
+    /// <param name="max">Верхняя граница заполнения</param>
+    /// <returns>Новый массив</returns>
     public static int[] CreateArray(int size, int min = 0, int max = 10)
     {
         return Enumerable.Range(1, size)
